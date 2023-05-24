082fec43b8a501009b2ce353df62e85fe7adebd4
ZhengXianda <xianda.zheng@jiamiantech.com>
2023-05-24 20:41:24 +0800

    实现Demo

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -2,6 +2,29 @@
 
 int main() 
 {
+  tkht::Debug = true;
+
+  tkgm::Load();
   
+  tkgui::Init(tkgm::video->window_width->Get(),
+              tkgm::video->window_height->Get(), "tksurvivor");
+
+  shared_ptr<tkgui::WindowMain> main_window = make_shared<tkgui::WindowMain>();
+  main_window->SetText("Welcome Survivor");
+  main_window->SetOptions({
+    {"New", [=](){ cout << "New" << endl; }},
+    {"Load", [=](){ cout << "Load" << endl; }},
+    {"Settings", [=](){ cout << "Settings" << endl; }},
+    {"About", [=](){ cout << "About" << endl; }},
+    {"Exit", [=](){ cout << "Exit" << endl; }},
+  });
+  main_window->UpdateSize(Width, Height);
+
+  tkgui::Display([=](){
+    main_window->Display();
+  });
+
+  tkgui::Exit();
+
   return 1;
 }
